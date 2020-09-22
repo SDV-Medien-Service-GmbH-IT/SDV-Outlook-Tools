46774c66cb345731e722488aadd954d5e24edb2e
Daniel Thaller <daniel_thaller@web.de>
2020-09-22 13:10:18 +0200

    Anpassungen an Ribbonbar, filter für Mailstatus und alter

---
+++ SDV-Outlook-Tools/Ribbon1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SDV_Outlook_Tools
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">"true", wenn verwaltete Ressourcen gelöscht werden sollen, andernfalls "false".</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl1 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl2 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl3 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl4 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl5 = this.Factory.CreateRibbonDropDownItem();
+            this.rb_SDVOutlookTools = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btn_RemoveAttachments = this.Factory.CreateRibbonButton();
+            this.dp_Mailstatus = this.Factory.CreateRibbonDropDown();
+            this.dp_Mailalter = this.Factory.CreateRibbonDropDown();
+            this.rb_SDVOutlookTools.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rb_SDVOutlookTools
+            // 
+            this.rb_SDVOutlookTools.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.rb_SDVOutlookTools.Groups.Add(this.group1);
+            this.rb_SDVOutlookTools.Label = "SDV-Outlook-Tools";
+            this.rb_SDVOutlookTools.Name = "rb_SDVOutlookTools";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.dp_Mailstatus);
+            this.group1.Items.Add(this.dp_Mailalter);
+            this.group1.Items.Add(this.btn_RemoveAttachments);
+            this.group1.Label = "Attachments";
+            this.group1.Name = "group1";
+            // 
+            // btn_RemoveAttachments
+            // 
+            this.btn_RemoveAttachments.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_RemoveAttachments.Image = ((System.Drawing.Image)(resources.GetObject("btn_RemoveAttachments.Image")));
+            this.btn_RemoveAttachments.Label = "Remove Attachments";
+            this.btn_RemoveAttachments.Name = "btn_RemoveAttachments";
+            this.btn_RemoveAttachments.ScreenTip = "Remove Attachments from Mails.";
+            this.btn_RemoveAttachments.ShowImage = true;
+            this.btn_RemoveAttachments.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_RemoveAttachments_Click);
+            // 
+            // dp_Mailstatus
+            // 
+            ribbonDropDownItemImpl1.Label = "gelesene";
+            ribbonDropDownItemImpl2.Label = "ungelesene";
+            this.dp_Mailstatus.Items.Add(ribbonDropDownItemImpl1);
+            this.dp_Mailstatus.Items.Add(ribbonDropDownItemImpl2);
+            this.dp_Mailstatus.Label = "Mailstatus";
+            this.dp_Mailstatus.Name = "dp_Mailstatus";
+            // 
+            // dp_Mailalter
+            // 
+            ribbonDropDownItemImpl3.Label = "30";
+            ribbonDropDownItemImpl4.Label = "60";
+            ribbonDropDownItemImpl5.Label = "90";
+            this.dp_Mailalter.Items.Add(ribbonDropDownItemImpl3);
+            this.dp_Mailalter.Items.Add(ribbonDropDownItemImpl4);
+            this.dp_Mailalter.Items.Add(ribbonDropDownItemImpl5);
+            this.dp_Mailalter.Label = "Zeitraum";
+            this.dp_Mailalter.Name = "dp_Mailalter";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.rb_SDVOutlookTools);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.rb_SDVOutlookTools.ResumeLayout(false);
+            this.rb_SDVOutlookTools.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab rb_SDVOutlookTools;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_RemoveAttachments;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dp_Mailstatus;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dp_Mailalter;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ SDV-Outlook-Tools/Ribbon1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SDV_Outlook_Tools
+{
+    public partial class Ribbon1
+    {
+        [System.ComponentModel.Browsable(false)]
+        public Microsoft.Office.Tools.Ribbon.RibbonComboBox SelectedItem { get; set; }
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            try
+            {
+                dp_Mailstatus.SelectedItemIndex =1;
+                dp_Mailalter.SelectedItemIndex = 2;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler im Programmablauf", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_RemoveAttachments_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                string Mailstatus = dp_Mailstatus.SelectedItem.ToString();
+                int Mailalter = Convert.ToInt32(dp_Mailalter.SelectedItem.ToString());
+                RemoveAttachments(Mailstatus, Mailalter);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler im Programmablauf", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void RemoveAttachments(string Mailstatus,int Mailalter)
+        {
+            try
+            {
+                DialogResult result1 = MessageBox.Show("Möchten Sie wirklich die Änhange aller "+ Mailstatus + " Mails älterer als " + Mailalter.ToString() + " Tage entfernen?", "Entfernen der Änhänge", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result1 == DialogResult.Yes)
+                {
+                    string pathToSave = getSaveFolder();
+                    if (pathToSave != "0")
+                    {
+                        EnumerateFoldersInDefaultStore(pathToSave, Mailstatus, Mailalter);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler im Programmablauf", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static string getSaveFolder()
+        {
+            try
+            {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog
+                {
+                    ShowNewFolderButton = true
+                };
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    return folderDlg.SelectedPath;
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "0";
+            }
+        }
+
+        static void EnumerateFoldersInDefaultStore(string pathToSaveFile, string Mailstatus, int Mailalter)
+        {
+            Microsoft.Office.Interop.Outlook.Application Application = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.Folder root = Application.Session.DefaultStore.GetRootFolder() as Microsoft.Office.Interop.Outlook.Folder;
+            EnumerateFolders(root, pathToSaveFile, Mailstatus, Mailalter);
+        }
+
+        static void EnumerateFolders(Microsoft.Office.Interop.Outlook.Folder folder, string pathToSaveFile, string Mailstatus, int Mailalter)
+        {
+            Microsoft.Office.Interop.Outlook.Folders childFolders = folder.Folders;
+            if (childFolders.Count > 0)
+            {
+                foreach (Microsoft.Office.Interop.Outlook.Folder childFolder in childFolders)
+                {
+                    //MessageBox.Show(childFolder.FolderPath);
+                    // We only want Inbox folders - ignore Contacts and others
+                    if (childFolder.FolderPath.Contains("Posteingang"))
+                    {
+                        // MessageBox.Show(childFolder.FolderPath);
+                        // Call EnumerateFolders using childFolder, to see if there are any sub-folders within this one
+                        EnumerateFolders(childFolder, pathToSaveFile, Mailstatus, Mailalter);
+                    }
+                }
+            }
+            //MessageBox.Show("Checking in " + folder.FolderPath);
+            IterateMessages(folder, pathToSaveFile, Mailstatus, Mailalter);
+        }
+
+        static void IterateMessages(Microsoft.Office.Interop.Outlook.Folder folder, string basePath, string Mailstatus, int Mailalter)
+        {
+            var fi=folder.Items; //= folder.Items.Restrict("[Unread] = true");
+                                 // Mailstatus
+                                 // Mailalter
+            if (Mailstatus== "ungelesene")
+            {
+                fi=folder.Items.Restrict("[Unread] = true");
+            }
+            else if (Mailstatus == "gelesene")
+            {
+                fi = folder.Items.Restrict("[Unread] = false" );
+            }
+      
+            if (fi != null)
+            {
+                foreach (Object item in fi)
+                {
+                    Microsoft.Office.Interop.Outlook.MailItem mi = (Microsoft.Office.Interop.Outlook.MailItem)item;
+                    var attachments = mi.Attachments;
+
+                    if (attachments.Count != 0)
+                    {
+                        // Create a directory to store the attachment 
+                        if (!Directory.Exists(basePath + folder.FolderPath))
+                        {
+                            Directory.CreateDirectory(basePath + folder.FolderPath);
+                        }
+                        if (mi.ReceivedTime < DateTime.Now.AddDays(-Mailalter))
+                        {
+                           MessageBox.Show(mi.Subject.ToString());
+                            if (mi.SenderEmailAddress != "wiki@sdv.de")
+                            {
+                                MessageBox.Show(mi.SenderEmailAddress);
+                                for (int i = 1; i <= mi.Attachments.Count; i++)
+                                {
+                                    var fn = mi.Attachments[i].FileName.ToLower();
+                                    // Create a further sub-folder for the sender
+                                    if (!Directory.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address))
+                                    {
+                                        Directory.CreateDirectory(basePath + folder.FolderPath + @"\" + mi.Sender.Address);
+                                    }
+                                    //totalfilesize = totalfilesize + mi.Attachments[i].Size;
+                                    if (!File.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName))
+                                    {
+                                        Console.WriteLine("Saving " + mi.Attachments[i].FileName);
+                                        mi.Attachments[i].SaveAsFile(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName);
+                                        mi.Body = mi.Body + "Anhange nach " + basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName + " gespeichert.";
+                                        //mi.Attachments[i].Delete();
+                                        mi.Save();
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Already saved " + mi.Attachments[i].FileName);
+                                    }
+                                    MessageBox.Show("Subject: " + mi.Subject + " Attachment: " + mi.Attachments[i].FileName);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+}
